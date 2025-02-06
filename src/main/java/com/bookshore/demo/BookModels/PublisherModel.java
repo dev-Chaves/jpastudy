e070a3f1a8788f3a7254c8fea254f76185326539
Joao V Chaves <chavesprogrammer@gmail.com>
2025-02-05 21:43:07 -0300

    model publisher adicionada

---
+++ src/main/java/com/bookshore/demo/BookModels/PublisherModel.java
@@ -0,0 +1,36 @@
+package com.bookshore.demo.BookModels;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Entity
+@Table(name = "TB_PUBLISHER")
+public class PublisherModel implements Serializable {
+
+    private static final long serialVersionUID = 1l;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
