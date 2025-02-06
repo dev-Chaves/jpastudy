49182ee0a6fd3b6b470fe880a29229673d77536d
Joao V Chaves <chavesprogrammer@gmail.com>
2025-02-05 21:43:26 -0300

    model review adicionada

---
+++ src/main/java/com/bookshore/demo/BookModels/ReviewModel.java
@@ -0,0 +1,37 @@
+package com.bookshore.demo.BookModels;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.rmi.server.UID;
+import java.util.UUID;
+
+@Entity
+@Table(name = "TBL_REVIEW")
+public class ReviewModel implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String review;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getReview() {
+        return review;
+    }
+
+    public void setReview(String review) {
+        this.review = review;
+    }
+}
