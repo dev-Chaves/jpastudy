f37122e61d1a4402d1039286cc5375c1325c1ee8
Joao V Chaves <chavesprogrammer@gmail.com>
2025-02-05 21:40:12 -0300

    first commit

---
+++ src/test/java/com/bookshore/demo/JpastudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookshore.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpastudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
