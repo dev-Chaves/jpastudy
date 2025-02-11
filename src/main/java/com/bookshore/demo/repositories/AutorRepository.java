9abe72fe65792700fcbd86870fd71dfa52945d1c
Joao V Chaves <chavesprogrammer@gmail.com>
2025-02-11 08:23:29 -0300

    autor/book repositories

--- src/main/java/com/bookshore/demo/repositories/AutorRepository.java
+++ src/main/java/com/bookshore/demo/repositories/AutorRepository.java
@@ -1,4 +1,13 @@
 package com.bookshore.demo.repositories;
 
-public interface AutorRepository {
+import com.bookshore.demo.models.AutorModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.UUID;
+
+public interface AutorRepository extends JpaRepository<AutorModel, UUID> {
+
+    AutorModel findAutorByName(String title);
+
 }
